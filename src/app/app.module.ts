e1f8a0a0d4dfaaea70cbd6824cb9f13009608a3f
fykidwai <f.y.kidwai@gmail.com>
2021-09-15 11:41:34 +0530

    adding employee-list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { FooterComponent } from "./companyPage/footerComponent/footer.component"
 import { HeaderComponent } from "./companyPage/header/header.component";
 import { EventBindingComponent } from './containers/event-binding/event-binding.component';
 import { ImplementBootstrapComponent } from './containers/implement-bootstrap/implement-bootstrap.component';
+import { EmployeeListComponent } from './containers/employee-list/employee-list.component';
 
 
 
@@ -14,7 +15,7 @@ import { ImplementBootstrapComponent } from './containers/implement-bootstrap/im
  */
 @NgModule({
     imports: [BrowserModule], // modules dependencies
-    declarations: [ContainerComponent, HeaderComponent, FooterComponent, ContentComponent, EventBindingComponent, ImplementBootstrapComponent], // Used to register components/services
+    declarations: [ContainerComponent, HeaderComponent, FooterComponent, ContentComponent, EventBindingComponent, ImplementBootstrapComponent, EmployeeListComponent], // Used to register components/services
     bootstrap: [ImplementBootstrapComponent] // container required at startup
 })
 export class AppModule { }
